11fe8d3fdd90ef54a575cc2a16ec314456c17c28
DevThatCodes <1steverchessguy@gmail.com>
2024-07-01 12:30:55 -0400

    made it easier to see name

--- booter.cpp
+++ booter.cpp
@@ -2,22 +2,39 @@
 #include <string>
 #include <fstream>
 
-
-
-int main() {
+int main()
+{
 
   std::string osOut;
   std::string osOutCl;
+  std::string name;
+  int dblQuotId;
+  bool namTim = false;
   std::ifstream osInfo("/etc/os-release");
 
-  while ( getline(osInfo, osOut) ) {
-    std::cout << osOut;
+  while (getline(osInfo, osOut))
+  {
     osOutCl += osOut + "\n";
   }
 
-  std::cout << "\n\n osOut:\n";
+  for (char &c : osOutCl)
+  {
+    if (c == '"')
+    {
+      c=' ';
+      dblQuotId += 1;
+      std::cout << name;
+      std::cout << "|\"|";
+      if (dblQuotId==4)
+      {
+        osOutCl = "\n------> " + name + " <------\n";
+      }
+      name = "";
+      name += c;
+    }
+  }
   std::cout << osOutCl;
 
-
+  osInfo.close();
   return 0;
 }
