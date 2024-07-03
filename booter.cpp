821515d1ea927f9bbb3a6b4767a3e89a89489baf
DevThatCodes <1steverchessguy@gmail.com>
2024-07-03 13:24:22 -0400

    updated gitingnore

--- booter.cpp
+++ booter.cpp
@@ -5,37 +5,7 @@
 int main()
 {
 
-  std::string osOut;
-  std::string osOutCl;
-  std::string name;
-  int dblQuotId;
-  std::ifstream osInfo("/etc/os-release");
-
-  while (getline(osInfo, osOut))
-  {
-    osOutCl += osOut + "\n";
-  }
-
-  for (char &c : osOutCl)
-  {
-    if (c == '"')
-    {
-      c=' ';
-      dblQuotId += 1;
-      std::cout << name;
-      std::cout << "|\"|";
-      if (dblQuotId==4)
-      {
-        std::cout << "\n------> " + name + " <------\n";
-        osOutCl = "\n------> " + name + " <------\n";
-      }
-      name = "";
-      name += c;
-    }
-  }
-  osInfo.close();
-  std::cout << osOutCl;
-
   
+
   return 0;
 }
