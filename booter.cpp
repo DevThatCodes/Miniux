debbe2d191bc3dc35898e716037d4c005ef14c44
DevThatCodes <1steverchessguy@gmail.com>
2024-07-03 14:24:49 -0400

    json drives as storage!!

--- booter.cpp
+++ booter.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "json/json.hpp"
+using json = nlohmann::json;
 
 int main()
 {
 
-  
+  std::ifstream f("test.json");
+  json drvDat = json::parse(f);
+  std::cout << "Welcome to " + (std::string) drvDat["osName"] << "\n";
+  std::cout << "Please choose a user from the list:";
+  std::cout << drvDat["users"];
 
   return 0;
 }
