a0720d5d84034168c5c68e2c4e592e0ee4ad530b
DevThatCodes <1steverchessguy@gmail.com>
2024-07-07 15:48:21 -0400

    Updated gitingore

--- booter.cpp
+++ booter.cpp
@@ -9,9 +9,38 @@ int main()
 
   std::ifstream f("test.json");
   json drvDat = json::parse(f);
+  std::string usrNm;
+  std::string inPswd;
+  std::vector<std::string> usrArr = {};
   std::cout << "Welcome to " + (std::string) drvDat["osName"] << "\n";
-  std::cout << "Please choose a user from the list:";
-  std::cout << drvDat["users"];
+  std::cout << "Please choose a user from the list:\n";
+  for (auto& usr : drvDat["users"].items()) {
+    std::cout << " " + usr.key() + "\n";
+    usrArr.push_back(usr.key());
+  }
 
+  // username input
+  std::cin >> usrNm;
+    for (int i=0; i<usrArr.size(); i++) {
+      if (usrArr[i] == usrNm){
+        break;
+      }
+      if (i == usrArr.size() -1 and usrArr[i] != usrNm) {
+        std::cout << "No user called \"" + usrNm + "\". Stopping system\n";
+        return 1;
+      }
+    }
+    std::cout << "Enter your password, " + usrNm + ": \n";
+    std::cin >> inPswd;
+
+    for (int j=0; j<usrArr.size(); j++) {
+      if (inPswd == drvDat["users"][usrNm]["password"]) {
+        std::cout << "Correct password, Welcome " + usrNm +"\n";
+        break;
+      } else if (inPswd != drvDat["users"][usrNm]["password"]) {
+        std::cout << "Password incorrect. Stopping system\n";
+        return 1;
+      }
+    }
   return 0;
 }
