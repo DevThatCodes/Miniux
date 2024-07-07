fb3c65dca334f7990e5d4b30ad7f3f8020f2412f
DevThatCodes <1steverchessguy@gmail.com>
2024-07-07 16:09:57 -0400

    boot sign in

--- booter.cpp
+++ booter.cpp
@@ -7,7 +7,7 @@ using json = nlohmann::json;
 int main()
 {
 
-  std::ifstream f("MiniuxOS/test.json");
+  std::ifstream f("MiniuxOS/Drive.json");
   json drvDat = json::parse(f);
   std::string usrNm;
   std::string inPswd;
